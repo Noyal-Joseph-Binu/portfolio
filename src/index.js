07ea22d1ebb19ff455a749142156bb5a150792a2
Noyal_Joseph_Binu <njosephbinu@hawk.iit.edu>
2026-02-09 14:29:11 -0600

    Restore React imports and rendering logic
    Uncommented React and rendering code, removed HTML redirect.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="refresh" content="0; url=https://noyaljosephbinu.vercel.app/" />
-    <script>
-      window.location.replace("https://noyaljosephbinu.vercel.app/");
-    </script>
-    <title>Redirecting…</title>
-  </head>
-  <body>
-    Redirecting to new site…
-  </body>
-</html>
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
